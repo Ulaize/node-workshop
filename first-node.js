69a21ca3902c1033adf3fe921ad5aa044d9194e7
Ulaize <ulaize@creative-tweed.com>
2015-09-16 19:19:31 +0000

    User location

--- first-node.js
+++ first-node.js
@@ -9,7 +9,13 @@ setTimeout(function(){
 
 //Find out latitude and longitude of the International Space Station
 
+Number.prototype.toRadians = function() {
+    return this * Math.PI / 180;
+}
+
 var request = require('request');
+
+var prompt = require('prompt');
 
 request("http://api.open-notify.org/iss-now.json", function(err, res, body) {
     if (!err) {
@@ -25,4 +31,42 @@ request("http://api.open-notify.org/iss-now.json", function(err, res, body) {
     else {
         console.log("there was an error: " + err);
     }
+    
 });
+
+
+
+//Calculation of distance between ISS and user's location
+
+
+ 
+  prompt.start();
+
+    console.log("What is your location");
+    
+  prompt.get(["location"], function (err, result) {
+      
+     var locationUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + result.location;
+     
+        request(locationUrl, function(err, res, body) {
+        if (!err) {
+            var userLocation = JSON.parse(body);
+            
+            var userLat = userLocation.results[0].geometry.location.lat;
+             var userLng = userLocation.results[0].geometry.location.lng;
+
+
+        console.log("Your exact location is " + userLat + "," + userLng);
+    }
+    else {
+        console.log("there was an error: " + err);
+    }
+    });
+  
+  
+});
+
+
+ 
+
+  
